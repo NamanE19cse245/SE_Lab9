cbb0ae77c7ebca8c2352ed26e482276c7b4889eb
Naman <e19cse245@bennett.edu.in>
2021-04-16 23:46:09 +0530

    Added assignment files

---
+++ jUnitFunction.java
@@ -0,0 +1,13 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	
+	public int addNumber(int a, int b) {
+		return a+b;
+	}
+	
+	public String addStrings(String a, String b) {
+		return a+b;
+	}
+	
+}
